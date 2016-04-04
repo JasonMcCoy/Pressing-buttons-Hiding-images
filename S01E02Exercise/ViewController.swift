6907b7c1b7b58406f81d89a1d1eca696debaf37b
Jason McCoy <brodooski@gmail.com>
2016-04-04 14:26:08 -0700

    My first upload to GitHub.

--- S01E02Exercise/ViewController.swift
+++ S01E02Exercise/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Images
+    @IBOutlet weak var blueBomb: UIImageView!
+    @IBOutlet weak var redBomb: UIImageView!
+    
+    // Buttons
+    @IBOutlet weak var hideBlueBombButton: UIButton!
+    @IBOutlet weak var hideRedBombButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+   
+    // If the blue button is pressed the red image should hide
+    @IBAction func hideBlueBombImage(sender: AnyObject) {
+        redBomb.hidden = true
+    }
 
+   
+    // If the red button is pressed the red image should hide
+    @IBAction func hideRedBombImage(sender: AnyObject) {
+        blueBomb.hidden = true
+    }
+    
 }
 
